66a0a994b1c1214b2de2550fdc6e3bf1b25d8ee8
youngran.kwon <youngran.kwon@lemonhc.com>
2021-05-10 09:03:35 +0900

    31. ModelMapper 적용

--- src/main/java/com/studyolle/settings/Profile.java
+++ src/main/java/com/studyolle/settings/Profile.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 @Data
-@NoArgsConstructor
 public class Profile {
 
     @Length(max = 35)
@@ -23,11 +22,4 @@ public class Profile {
 
     private String profileImage;
 
-    public Profile(Account account) {
-        this.bio = account.getBio();
-        this.url = account.getUrl();
-        this.occupation = account.getOccupation();
-        this.location = account.getLocation();
-        this.profileImage = account.getProfileImage();
-    }
 }
