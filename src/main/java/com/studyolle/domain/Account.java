3d81e333e8552f9714c7796cffd8cc412016ae9f
youngran.kwon <youngran.kwon@lemonhc.com>
2021-05-12 11:56:52 +0900

    35. 관심 주제 도메인

--- src/main/java/com/studyolle/domain/Account.java
+++ src/main/java/com/studyolle/domain/Account.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -58,6 +59,9 @@ public class Account {
     private boolean studyUpdatedByEmail;
 
     private boolean studyUpdatedByWeb = true;
+
+    @ManyToMany
+    private Set<Tag> tags;
 
     public void generateEmailCheckToken() {
         this.emailCheckToken = UUID.randomUUID().toString();
