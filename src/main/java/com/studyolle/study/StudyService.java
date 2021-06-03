6903f5cc3951e0352f41487338dd13d882bbb003
youngran.kwon <youngran.kwon@lemonhc.com>
2021-06-03 11:53:57 +0900

    58. 스터디 설정 - 경로 및 이름 수정

--- src/main/java/com/studyolle/study/StudyService.java
+++ src/main/java/com/studyolle/study/StudyService.java
@@ -11,6 +11,8 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static com.studyolle.study.form.StudyForm.VALID_PATH_PATTERN;
+
 
 @Service
 @Transactional
@@ -119,5 +121,23 @@ public class StudyService {
         study.stopRecruit();
     }
 
+    public boolean isValidPath(String newPath) {
+        if (!newPath.matches(VALID_PATH_PATTERN)) {
+            return false;
+        }
 
+        return !studyRepository.existsByPath(newPath);
+    }
+
+    public void updateStudyPath(Study study, String newPath) {
+        study.setPath(newPath);
+    }
+
+    public boolean isValidTitle(String newTitle) {
+        return newTitle.length() <= 50;
+    }
+
+    public void updateStudyTitle(Study study, String newTitle) {
+        study.setTitle(newTitle);
+    }
 }
