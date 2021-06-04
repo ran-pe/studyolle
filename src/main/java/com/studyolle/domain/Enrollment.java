d95264431064a257f5160870333f44bab3e6e5dc
youngran.kwon <youngran.kwon@lemonhc.com>
2021-06-04 12:08:48 +0900

    62. 모임 도메인

---
+++ src/main/java/com/studyolle/domain/Enrollment.java
@@ -0,0 +1,32 @@
+package com.studyolle.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(of="id")
+public class Enrollment {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    private Event event;
+
+    @ManyToOne
+    private Account account;
+
+    private LocalDateTime enrolledAt;
+
+    private boolean accepted;
+
+    private boolean attended;
+
+}
---
+++ src/main/java/com/studyolle/domain/EventType.java
@@ -0,0 +1,6 @@
+package com.studyolle.domain;
+
+public enum EventType {
+
+    FCFS, CONFIRMATIVE;
+}
