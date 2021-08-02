09e2678974a50375c6f96b7068433ff051c15f8f
youngran.kwon <youngran.kwon@lemonhc.com>
2021-08-02 10:59:40 +0900

    81. 모임 관련 알림

---
+++ src/main/java/com/studyolle/modules/event/event/EnrollmentEvent.java
@@ -0,0 +1,14 @@
+package com.studyolle.modules.event.event;
+
+import com.studyolle.modules.event.Enrollment;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public abstract class EnrollmentEvent {
+
+    protected final Enrollment enrollment;
+
+    protected final String message;
+}
